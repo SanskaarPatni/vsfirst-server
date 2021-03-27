d258645adaf3489560f91c6cde824f1d8b0d6981
Sanskaar Patni <patnisanskaar@gmail.com>
2021-03-27 23:17:41 +0530

    vs first backend

---
+++ src/types/env.d.ts
@@ -0,0 +1,10 @@
+//generated using Steps
+//1 Make types folder in src then
+//2 npx gen-env-types .env -o src/types/env.d.ts -e  .
+declare namespace NodeJS {
+  interface ProcessEnv {
+    GITHUB_CLIENT_SECRET: string;
+    GITHUB_CLIENT_ID: string;
+    ACCESS_TOKEN_SECRET: string;
+  }
+}
